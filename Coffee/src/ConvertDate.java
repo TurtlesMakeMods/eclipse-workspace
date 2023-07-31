147dabb21edb2cb4c28673f3b633acefea23d6c7
S26600478 <S26600478@10.106.185.196>
2023-07-31 15:30:16 -0700

    ConvertDate :D

--- Coffee/src/ConvertDate.java
+++ Coffee/src/ConvertDate.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
 
 public class ConvertDate {
-
+	public static final int JANUARY = 31;
+	public static final int FEBRUARY = 28;
+	public static final int FEBRUARY_LEAP_YEAR = 29;
+	public static final int MARCH = 31;
+	public static final int APRIL = 30;
+	public static final int MAY = 31;
+	public static final int JUNE = 30;
+	public static final int JULY = 31;
+	public static final int AUGUST = 31;
+	public static final int SEPTEMBER = 30;
+	public static final int OCTOBER = 31;
+	public static final int NOVEMBER = 30;
+	public static final int DECEMBER = 31;
+	
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int monthNumber;
+		int monthName;
+		int day;
+		String input;
 		
-
+		System.out.println("Enter a date in MM/DD/YYYY format");
+		input = scan.nextLine();
+		
+		
+		if (input.substring(0,1).equals("0"))
+			monthNumber = Integer.parseInt(input.substring(0,2));
+		else
+			monthNumber = Integer.parseInt(input.substring(0,1));
+		
+		System.out.println(monthNumber);
+		//if ()
 	}
 
 }
